e928b49ae9780d6310ddd3d409ab1c98ed0da488
Dominik Salvet <dominik.salvet@gmail.com>
2022-11-05 12:48:57 +0100

    add swr namespace

--- swrast/swrast.cpp
+++ swrast/swrast.cpp
@@ -1,10 +1,11 @@
 // https://github.com/dominiksalvet/swrast
 
 #include "swrast.hpp"
-
 #include <iostream>
 
-void test()
+using namespace swr;
+
+void swr::test()
 {
     std::cout << "hello from test()" << std::endl;
 }
